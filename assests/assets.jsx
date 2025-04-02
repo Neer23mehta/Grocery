1d7c66a7e693c6ca697075dc10439b6959e68aa0
Neer23mehta <mehtaneer143@gmail.com>
2025-04-02 18:52:05 +0530

    new

---
+++ assests/assets.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import bgimg from "../assests/Group 1491/Group 1491@2x.jpg"
+import limg from "../assests/Group 1600.svg"
+import left from "../assests/Group 1600 (3)/Group 1600.png"
+import logo from "../assests/Path 410/Path 410.jpg"
+import sym from "../assests/Group 331/Group 331.jpg"
+import lock from "../assests/Group 336/Group 336.jpg"
+import mail from "../assests/Group 335/Group 335.jpg"
+import rimg from "../assests/Group 341 (2)/Group 341.png"
+import eye from "../assests/Group 334/Group 334.jpg"
+import pass from "../assests/Group 331 (2)/Group 331.png"
+import back from "../assests/back-icon/back-icon.png"
+import faq from "../assests/Group 1685/Group 1685.png"
+import page from "../assests/Group 1464 (7)/Group 1464.png"
+import hm from "../assests/Group 1464 (6)/Group 1464.png"
+import brands from "../assests/Group 1464 (5)/Group 1464.png"
+import category from "../assests/Group 1464 (4)/Group 1464.png"
+import products from "../assests/Group 1464 (2)/Group 1464.png"
+import order from "../assests/Group 1464 (3)/Group 1464.png"
+import user from "../assests/Group 1464 (1)/Group 1464.png"
+import dash from "../assests/Group 1464/Group 1464.png"
+import nlogo from "../assests/Path 410 (1)/Path 410.png"
+import coupon from "../assests/Icons/Icons.png"
+import bimg from "../assests/back-icon (2)/back-icon.png"
+import eyeClosed from "../assests/Group 334 (1)/Group 334.png"
+
+export const assets = {
+    bgimg,
+    limg,
+    left,
+    logo,
+    nlogo,
+    sym,
+    lock,
+    mail,
+    rimg,
+    eye,
+    pass,
+    back,
+    faq,
+    page,
+    hm,
+    brands,
+    category,products,order,user,dash,coupon,bimg,eyeClosed
+};

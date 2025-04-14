de838807e53fd13be40a3688460de32c4be93c1b
Neer23mehta <mehtaneer143@gmail.com>
2025-04-14 17:49:46 +0530

    api

--- commonapi/Commonapi.tsx
+++ commonapi/Commonapi.tsx
@@ -1,19 +1,62 @@
+'use client'
 import axios from 'axios';
-import React from 'react'
 
-export default async function commonGetApis(Params:any) {
-    const refreshtoken = localStorage.getItem("usertoken");
-    const token = localStorage.getItem("token");    
-    try {
-        const res = await axios.get(`http://192.168.2.181:3000/admin/${Params}`,{
-            headers:{
-                Authorizations:token,
-                language:"en",
-                refresh_token:refreshtoken
-            }
-        })
-        return res.data
-    } catch (error) {
-        
-    }
+const commonGetApis = async (params: string) => {
+  const refreshtoken = typeof window !== "undefined" ? localStorage.getItem("usertoken") : null;
+  const token = typeof window !== "undefined" ? localStorage.getItem("token") : null;
+
+  try {
+    const res = await axios.get(`http://192.168.2.181:3000/admin/${params}`, {
+      headers: {
+        Authorizations: token,
+        language: "en",
+        refresh_token: refreshtoken
+      }
+    });
+    return res.data;
+  } catch (error) {
+    console.error("API GET Error:", error);
+    throw error;
+  }
+
+};
+
+export const deleteApi = async (params: string) => {
+  const token = typeof window !== "undefined" ? localStorage.getItem("token") : null;
+  const refreshtoken = typeof window !== "undefined" ? localStorage.getItem("usertoken") : null;
+
+  try {
+    const res = await axios.delete(`http://192.168.2.181:3000/admin/${params}`, {
+      headers: {
+        Authorizations: token,
+        language: "en",
+        refresh_token: refreshtoken
+      }
+    });
+    return res.data;
+  } catch (error) {
+    console.error("API GET Error:", error);
+    throw error;
+  }
 }
+
+export const commonPostApis = async (params:string) => {
+  const token = typeof window !== "undefined" ? localStorage.getItem("token") : null;
+  const refreshtoken = typeof window !== "undefined" ? localStorage.getItem("usertoken") : null;
+
+  try {
+    const res = await axios.post(`http://192.168.2.181:3000/admin/${params}`,{
+      headers: {
+        Authorization: token,
+        language: "en",
+        refresh_token: refreshtoken
+      }
+    });
+    return res.data
+  } catch (error) {
+    console.error("Add Api is not Working");
+    throw error;
+  }
+}
+
+export default commonGetApis;
